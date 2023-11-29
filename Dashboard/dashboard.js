a4846d1e57d6a43b8f861e8e9137be002667f3ee
mikemills254 <mikemills@gmail.com>
2023-11-29 18:39:42 +0300

    updates

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -1,7 +1,44 @@
 document.getElementById('onboard-btn').addEventListener('click', async () => {
-    window.location = "./onboardhospital.html"
-})
+    window.location = "./onboardhospital.html";
+});
 
 document.getElementById('dashboard-btn').addEventListener('click', () => {
-    window.location = './dashboard.html'
-})
+    window.location = './dashboard.html';
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+    console.log('loaded');
+    try {
+        const response = await fetch('https://firebasedata.herokuapp.com/api/v1/registerHospital', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.status === 200) {
+            let responseData = await response.json();
+
+            // Check if 'data' key exists and is an array
+            const jsonData = responseData.data || [];
+
+            jsonData.forEach(element => {
+                var body = document.createElement('tr');
+
+                body.innerHTML = `
+                    <td>${element.name}</td>
+                    <td>${element['reg-number']}</td>
+                    <td>${element.location}</td>
+                    <td>${element.phone}</td>
+                    <td>${element.email}</td>
+                    <td>${element.country}</td>
+                `;
+
+                // Append to table
+                document.getElementById('hospitalTableBody').appendChild(body);
+            });
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
